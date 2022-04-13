f24bd34d31e42145ab5717ca811971836f2b6643
Victoria Gonzalez <gonzalezevic@gmail.com>
2022-04-13 11:36:32 -0700

    commit tree ac6a7d1f8cf3a78bd1326b2296aeb03da97d5d30

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,14 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{
+    <li key= {links}>
+    <a href="#home">home</a>
+    <a href="#about">about</a>
+    <a href="#projects">projects</a>
+    </li>
+   }</nav>;
+   
 }
 
 export default NavBar;
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,13 +1,14 @@
 import React from "react";
 
-function ProjectItem({ name, about, technologies }) {
+function ProjectItem({ project:{name, about, technologies} }) {
+  const technologyList = technologies.map((technologies)=> {
+    <span key={technologies}>{technologies}</span>
+  })
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
-      <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
-      </div>
+      <div className="technologies">{technologyList}</div>
     </div>
   );
 }
